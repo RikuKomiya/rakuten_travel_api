8425e3f2e2d8b2d749d8d5cf7baa8fbf4323408b
RikuKomiya <824rikukomiya@gmail.com>
2023-02-18 00:19:54 +0900

    applicationId

--- spec/rakute_travel_api/client_spec.rb
+++ spec/rakute_travel_api/client_spec.rb
@@ -2,4 +2,34 @@ RSpec.describe RakutenTravelApi::Client do
   it "can be initizalie" do
     expect { RakutenTravelApi::Client.new }.not_to raise_error
   end
+
+  describe "#keyword_hotel_search" do
+    before do
+      RakutenTravelApi.configure do |config|
+        config.application_id = "applicationId"
+      end
+    end
+    context "when request is successful", :vcr do
+      let(:client) { RakutenTravelApi::Client.new }
+      let(:response) { client.keyword_hotel_search(keyword: "東京") }
+      let(:first_hotel) { response.hotels.first }
+
+      it "returns response" do
+        expect(response).to be_an_instance_of(RakutenTravelApi::Response)
+      end
+
+      it "returns an array of hotel" do
+        expect(first_hotel).to be_an_instance_of(RakutenTravelApi::Hotel)
+      end
+    end
+
+    context "when request is failed", :vcr do
+      let(:client) { RakutenTravelApi::Client.new }
+      let(:response) { client.keyword_hotel_search(keyword: "hogehoge") }
+
+      it "returns an array" do
+        expect { response }.to raise_error RakutenTravelApi::Errors::NotFoundError
+      end
+    end
+  end
 end
