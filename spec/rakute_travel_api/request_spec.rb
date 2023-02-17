07cd0f32994211de00ecf53c04e67e42d6c48622
RikuKomiya <824rikukomiya@gmail.com>
2023-02-18 00:04:21 +0900

    change request_spec

--- spec/rakute_travel_api/request_spec.rb
+++ spec/rakute_travel_api/request_spec.rb
@@ -1,27 +1,31 @@
 RSpec.describe RakutenTravelApi::Request do
-  before do
-    RakutenTravelApi.configure do |c|
-      c.application_id = "applicationId"
+  describe "#send_request" do
+    before do
+      RakutenTravelApi.configure do |c|
+        c.application_id = "application_id"
+      end
     end
-  end
-
-  let(:path) { "/services/api/Travel/KeywordHotelSearch/20170426" }
-  subject { RakutenTravelApi::Request.new(path, keyword: keyword).send_request }
-
-  context "success", vcr: { cassette_name: :search_hotel_keyword_success } do
-    let(:keyword) { "新宿" }
-
-    it "can send request" do
-      expect(subject["hotels"][0]["hotel"][0]["hotelBasicInfo"]["address1"]).to eq "東京都"
+    let(:params) do
+      RakutenTravelApi::Parameter.new({ keyword: keyword }).to_params_for_keyword_search
     end
-  end
 
-  context "error" do
-    context "return 404", vcr: { cassette_name: :search_hotel_keyword_not_data } do
-      let(:keyword) { "null" }
+    context "when request is success", :vcr do
+      let(:path) { "/services/api/Travel/KeywordHotelSearch/20170426" }
+      let(:keyword) { "新宿" }
+      subject { RakutenTravelApi::Request.new(path: path, params: params) }
 
-      it "raise NotFoundError" do
-        expect { subject }.to raise_error RakutenTravelApi::Errors::NotFoundError
+      it "return response" do
+        expect(subject.send_request).to be_a RakutenTravelApi::Response
+      end
+    end
+
+    context "when request is failed", :vcr do
+      let(:path) { "/services/api/Travel/KeywordHotelSearch/20170426" }
+      let(:keyword) { "hogehoge" }
+      subject { RakutenTravelApi::Request.new(path: path, params: params) }
+
+      it "raise error" do
+        expect { subject.send_request }.to raise_error RakutenTravelApi::Errors::NotFoundError
       end
     end
   end
